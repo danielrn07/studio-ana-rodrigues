b5d6ac1ea12cc39cb7e0aa682f42dd0fd7af875c
Daniel Nascimento <pdaniel.contato@gmail.com>
2023-10-03 20:57:21 -0300

    feat: add our services

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,24 @@
 function state() {
   const openMenu = false
-  return { menuSideBar: false, openMenu }
+  const services = [
+    {
+      img: '/assets/img/ic_cabelos.png',
+      title: 'Arte em cabelos',
+      description: 'Criamos cortes e estilos que\nrealçam sua beleza única.',
+    },
+    {
+      img: '/assets/img/ic_unhas.png',
+      title: 'Unhas impecáveis',
+      description:
+        'Cuidaremos das suas unhas,\nproporcionando-lhes um toque\nde elegância e sofisticação.',
+    },
+    {
+      img: '/assets/img/ic_sobrancelhas.png',
+      title: 'Olhar encantador',
+      description:
+        'Moldamos suas sobrancelhas\ncom precisão para realçar\na beleza natural dos seus.',
+    },
+  ]
+
+  return { menuSideBar: false, openMenu, services }
 }
